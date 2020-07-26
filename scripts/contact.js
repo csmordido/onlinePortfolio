f0d45edd4216e4d5d412e2d379a9691d72e47ccb
Cheriele Mordido <eyelmordido@gmail.com>
2020-07-25 20:11:38 -0400

    added form validation

--- scripts/contact.js
+++ scripts/contact.js
@@ -3,6 +3,8 @@ const contact = {};
 contact.nameInput = document.getElementById("name");
 contact.emailInput = document.getElementById("email");
 contact.messageInput = document.getElementById("message");
+contact.formInputs = document.querySelectorAll("form input, form textarea");
+contact.inValid;
 
 contact.scriptURL = "https://script.google.com/macros/s/AKfycbz5osV3SwJZLj74KMB5P9CzWMNl7KdFTVjDIN0anLv43eSbpnb4/exec";
 
@@ -10,56 +12,58 @@ contact.form = document.forms["submit-to-google-sheet"];
 
 contact.form.addEventListener("submit", e => {
   e.preventDefault()
-  fetch(scriptURL, { method: "POST", body: new FormData(contact.form)})
+
+  fetch(contact.scriptURL, { method: "POST", body: new FormData(contact.form)})
     .then(response => console.log("Success!", response))
     .catch(error => console.error("Error!", error.message));
 });
 
-// contact.form.addEventListener("input", (event) => {
-//   // this = event.target;
-//   console.log(event.target.value);
-//   // const validity = this.checkValidity();
-  
-// });
-
-// contact.validate = (input, errorMessage) => {
-//   if (!input.checkValidity() || !input.value) {
-//     errorMessage.style.visibility = "visible";
-//   } else {
-//     errorMessage.style.visibility = "hidden";
-//   }
-// }
-
-contact.nameInput.addEventListener("input", () => {
+contact.nameInput.addEventListener("keyup", function() {
+  contact.enableSubmit();
   const errorMessage = document.querySelector("#name + p");
-  if (!contact.nameInput.checkValidity() || !contact.nameInput.value.trim()) {
+  if (!this.checkValidity() || !this.value.trim()) {
     errorMessage.style.visibility = "visible";
   } else {
     errorMessage.style.visibility = "hidden";
   }
 });
 
-contact.emailInput.addEventListener("input", () => {
-  // const emailInputValue = document.getElementById("email").value;
+contact.emailInput.addEventListener("keyup", function() {
+  contact.enableSubmit();
   const validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const errorMessage = document.querySelector("#email + p");
-  if (!contact.emailInput.checkValidity() || !validEmail.test(contact.emailInput.value)) {
+  if (!this.checkValidity() || !validEmail.test(this.value)) {
     errorMessage.style.visibility = "visible";
   } else {
     errorMessage.style.visibility = "hidden";
   }
 });
 
-contact.messageInput.addEventListener("input", () => {
+contact.messageInput.addEventListener("keyup", function() {
+  contact.enableSubmit();
   const errorMessage = document.querySelector("#message + p");
-  if (!contact.messageInput.checkValidity() || !contact.messageInput.value.trim()) {
+  if (!this.checkValidity() || !this.value.trim()) {
     errorMessage.style.visibility = "visible";
   } else {
     errorMessage.style.visibility = "hidden";
   }
 });
 
+contact.enableSubmit = function() {
+  const formSubmitBtn = document.getElementsByClassName("formBtn");
+  const classes = formSubmitBtn[0].classList;
 
-// contact.validateName();
-// contact.validateEmail();
-// contact.validateMessage();
+  contact.formInputs.forEach( object => {
+    if (!object.checkValidity()) {
+      contact.inValid = true;
+    } else {
+      contact.inValid = false;
+    }
+  });
+
+  if (!contact.inValid) {
+    formSubmitBtn[0].removeAttribute("disabled");
+    classes.remove("disabled");
+  }
+};
+
