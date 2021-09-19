46f53c59402b59685163b81973fa5ca10e827599
Eyel Mordido <eyel@agilehumanities.ca>
2021-09-19 00:19:26 -0400

    updated main width and padding and fixed changeTheme.js

--- scripts/changeTheme.js
+++ scripts/changeTheme.js
@@ -1,27 +1,32 @@
-const app = {};
-
-app.changeTheme = function() {
-  const darkBtn = document.querySelector('.dark');
-  const lightBtn = document.querySelector('.light');
-
-  function changeThemeOnClick(btn, bgColourVal, txtColourVal) {
-    
-    btn.addEventListener('click', function() {
-      let bgColour = getComputedStyle(document.documentElement).getPropertyValue('--bg-colour');
-      document.documentElement.style.setProperty('--bg-colour', bgColourVal);
-      document.documentElement.style.setProperty('--text-colour', txtColourVal);
-      
-      if (bgColour === '#d9cdf8') {
-        document.body.classList.add('dark');
-      } else {
-        document.body.classList.remove('dark');
-      }
-    });
-  }
-
-  changeThemeOnClick(darkBtn, '#351d7b', '#a68af5');
-  changeThemeOnClick(lightBtn, '#d9cdf8', '#532cbf');
+document.addEventListener("DOMContentLoaded", function() {
+  const app = {};
   
-}
+  app.changeTheme = function() {
+    const darkBtn = document.querySelector('.dark');
+    const lightBtn = document.querySelector('.light');
+    const body = document.querySelector('body');
 
-app.changeTheme();
+    function changeThemeOnClick(btn, bgColourVal, txtColourVal) {
+      
+      btn.addEventListener('click', function() {
+        document.documentElement.style.setProperty('--bg-colour', bgColourVal);
+        document.documentElement.style.setProperty('--text-colour', txtColourVal);
+
+        let bgColour = getComputedStyle(document.documentElement).getPropertyValue('--bg-colour');
+        
+        if (bgColour === '#351d7b') {
+          body.classList.add('dark-theme');
+        } else {
+          body.classList.remove('dark-theme');
+        }
+      
+      });
+    }
+  
+    changeThemeOnClick(darkBtn, '#351d7b', '#a68af5');
+    changeThemeOnClick(lightBtn, '#d9cdf8', '#532cbf');
+    
+  }
+  
+  app.changeTheme();
+});
