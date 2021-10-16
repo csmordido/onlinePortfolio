8f0147ee2bfb04aa80611189e4c68c7cc58dd5aa
Eyel Mordido <eyel@agilehumanities.ca>
2021-10-16 15:49:00 -0400

    added classes and updated styles of intro and work sections to animate swtiching of pages

--- scripts/toggleSectionsVisibility.js
+++ scripts/toggleSectionsVisibility.js
@@ -3,13 +3,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
   page.workLink = document.querySelector('.work-link');
   page.workSection = document.getElementById('work');
+  page.homeLink = document.querySelector('.home-link');
   page.introSection = document.getElementById('intro');
+
+  page.switchPages = function(linkEl) {
+
+    linkEl.addEventListener('click', function(event) {
   
-  page.workLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    page.workSection.classList.remove('work-hidden');
-    page.introSection.classList.add('intro-hidden');
-    
-  });
+      event.preventDefault();
+
+      if (this.classList.contains('work-link')) {
+
+        document.body.classList.add('unset-overflow');
+        page.introSection.classList.remove('intro-visible');
+        page.workSection.classList.add('work-visible');
+
+      } else {
+
+        document.body.classList.remove('unset-overflow');
+        page.introSection.classList.add('intro-visible');
+        page.workSection.classList.remove('work-visible');
+
+      };
+      
+    });
+
+  }
+
+  page.switchPages(page.workLink);
+  page.switchPages(page.homeLink);
 
 });
