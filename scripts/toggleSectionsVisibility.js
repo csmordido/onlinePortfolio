6705bea04e372bd03d31d38a296e0e86e6bb7acf
Eyel Mordido <eyel@agilehumanities.ca>
2021-10-16 17:09:15 -0400

    added functionality of moving link rectangle when switching pages

--- scripts/toggleSectionsVisibility.js
+++ scripts/toggleSectionsVisibility.js
@@ -5,12 +5,25 @@ document.addEventListener("DOMContentLoaded", function() {
   page.workSection = document.getElementById('work');
   page.homeLink = document.querySelector('.home-link');
   page.introSection = document.getElementById('intro');
+  page.rectangle = document.querySelector('.rectangle');
+
+  page.moveRectangle = function(el) {
+    
+    let linkWidth = el.offsetWidth;
+    let leftPosition = el.offsetLeft;
+
+    document.documentElement.style.setProperty('--link-width', `${linkWidth}px`);
+    document.documentElement.style.setProperty('--rectangle-left-pos', `${leftPosition}px`);
+
+  }
 
   page.switchPages = function(linkEl) {
 
     linkEl.addEventListener('click', function(event) {
-  
+      
       event.preventDefault();
+
+      page.moveRectangle(this);
 
       if (this.classList.contains('work-link')) {
 
